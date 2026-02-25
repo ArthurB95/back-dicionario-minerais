0b44aed78b23b7a2c3543f53694876a36d90e6ba
Arthur Bicalho <arthurbic95@gmail.com>
2026-02-25 01:03:13 -0300

    mudança do origins

--- src/main/java/com/example/dicionariominerais/controller/HealthCheckController.java
+++ src/main/java/com/example/dicionariominerais/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"${frontend.staging}"})
+@CrossOrigin(origins = {"${frontend.master}"})
 @RestController
 @RequestMapping("/api/health")
 public class HealthCheckController {
