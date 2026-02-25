8c5260dcb8806fc9f601fd3397123259c17b56ae
Arthur Bicalho <arthurbic95@gmail.com>
2026-02-24 23:24:44 -0300

    Novo endpoint do macro desenvolvido

---
+++ src/main/java/com/example/dicionariominerais/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.example.dicionariominerais.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {"${frontend.staging}"})
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("Servidor online e acordado!");
+    }
+}
---
+++ src/main/java/com/example/dicionariominerais/controller/MacroMineralsController.java
@@ -0,0 +1,38 @@
+package com.example.dicionariominerais.controller;
+
+import com.example.dicionariominerais.entity.MacroMinerals;
+import com.example.dicionariominerais.repository.MacroMineralsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = {"${frontend.staging}"})
+@RestController
+@RequestMapping("/macro-minerals")
+public class MacroMineralsController {
+
+    @Autowired
+    private MacroMineralsRepository macroMineralsRepository;
+
+    @PostMapping
+    public ResponseEntity<MacroMinerals> criarMacroMineral(@RequestBody MacroMinerals mineral) {
+        MacroMinerals salvo = macroMineralsRepository.save(mineral);
+        return ResponseEntity.ok(salvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MacroMinerals>> listarMacroMinerais() {
+        List<MacroMinerals> minerais = macroMineralsRepository.findAll();
+        return ResponseEntity.ok(minerais);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MacroMinerals> buscarPorIdMacroMinerals(@PathVariable Long id) {
+        Optional<MacroMinerals> mineral = macroMineralsRepository.findById(id);
+        return mineral.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
